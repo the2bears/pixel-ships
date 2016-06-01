50a7624b45dfdcb3677d0ffc744a1da0b41262ca
William Swaney <the2bears@gmail.com>
2016-05-31 19:53:20 -0700

    First re-factoring for the new data format.

--- src/pixel_ships/core.clj
+++ src/pixel_ships/core.clj
@@ -1,67 +1,75 @@
 (ns pixel-ships.core)
 
-(declare color-from-coords)
-
 (def n 12)
 (def pixels-per-cell 3)
-(def always-solid '((5 2) (5 3) (5 4) (5 5) (5 9)))
-(def always-solid-tagged (map #(list (first %) (second %) :solid) always-solid))
+(def always-solid2 {:pixels {:solid '([5 2] [5 3] [5 4] [5 5] [5 9])}})
 
 (def hull-cols '(4 5 4 3 4 3 4 2 3 4 1 2 3 1 2 3 1 2 3 1 2 3 4  3  4  5))
 (def hull-rows '(1 1 2 3 3 4 4 5 5 5 6 6 6 7 7 7 8 8 8 9 9 9 9 10 10 10))
 (def hull-count (count hull-cols))
 (def hull (map list hull-cols hull-rows))
-(defn index-of-hull [x y]
-  (.indexOf hull [x y]))
-
-(defn is-hull? [x y]
-  (if (< (index-of-hull x y) 0) false
-    true))
-
-
 
 (def cockpit-cols '(4 5 4 5 4 5))
 (def cockpit-rows '(6 6 7 7 8 8))
 (def cockpit (map list cockpit-cols cockpit-rows))
 (def cockpit-count (count cockpit-cols))
 
-(defn add-hull [cells seed]
-  (letfn [(checker [result i mask]
+
+(defn add-hull2 [cells]
+  (let [seed (:seed cells)
+        checker (fn [result i mask]
                    (cond (>= i hull-count) result
                          (not= 0 (bit-and seed mask))
-                           (recur (conj result (list (nth hull-cols i) (nth hull-rows i) :hull)) (inc i) (bit-shift-left mask 1))
+                           (recur (conj result (vector (nth hull-cols i) (nth hull-rows i))) (inc i) (bit-shift-left mask 1))
                          :else (recur result (inc i) (bit-shift-left mask 1))))]
-    (checker cells 0 1)))
+    (assoc-in cells [:pixels :hull] (checker () 0 1))))
 
-(defn add-cockpit [cells seed]
-  (let [checker (fn [result i mask]
-                   (cond (>= i cockpit-count) result
+(defn add-cockpit2 [cells]
+  (let [seed (:seed cells)
+        checker (fn [solid-cells cockpit-cells i mask]
+                   (do
+                     (prn "Cockpit" cockpit-cells)
+                     (prn "Solid" solid-cells)
+                   (cond (>= i cockpit-count)
+                           (let [pixels (:pixels cells)]
+                                 (assoc cells :pixels (merge pixels (assoc {} :cockpit cockpit-cells) (assoc {} :solid solid-cells))))
                          (not= 0 (bit-and seed mask))
-                           (recur (conj result (list (nth cockpit-cols i) (nth cockpit-rows i) :solid)) (inc i) (bit-shift-left mask 1))
-                         :else (recur (conj result (list (nth cockpit-cols i) (nth cockpit-rows i) :cockpit)) (inc i) (bit-shift-left mask 1))))]
-    (checker cells 0 (bit-shift-left 1 26))))
+                           (recur (conj solid-cells (vector (nth cockpit-cols i) (nth cockpit-rows i))) cockpit-cells (inc i) (bit-shift-left mask 1))
+                         :else (recur solid-cells (conj cockpit-cells (vector (nth cockpit-cols i) (nth cockpit-rows i))) (inc i) (bit-shift-left mask 1)))))]
+    (checker ((comp :solid :pixels) cells) ((comp :cockpit :pixels) cells) 0 (bit-shift-left 1 26))))
 
-(defn wrap-with-solids [cells]
-  (let [cells-no-tag (map #(list (first %) (second %)) cells)
+(defn wrap-with-solids2 [cells]
+  (let [hull-cells (into [] ((comp :hull :pixels) cells))
+        all-cells (into [] (reduce #(concat %1 %2) (vals (:pixels cells))))
         checker (fn [result x y]
                   (cond (>= x (/ n 2)) (recur result 0 (inc y))
-                        (>= y n) result
-                        (and (or (.contains cells (list x (inc y) :hull))
-                                 (.contains cells (list x (dec y) :hull))
-                                 (.contains cells (list (inc x) y :hull))
-                                 (.contains cells (list (dec x) y :hull)))
-                             (not (.contains cells-no-tag (list x y))))
-                          (recur (conj result (list x y :solid)) (inc x) y)
+                        (>= y n) (assoc-in cells [:pixels :solid] (distinct result))
+                        (and (or (.contains hull-cells (vector x (inc y) ))
+                                 (.contains hull-cells (vector x (dec y) ))
+                                 (.contains hull-cells (vector (inc x) y ))
+                                 (.contains hull-cells (vector (dec x) y )))
+                             (not (.contains all-cells (vector x y))))
+                          (recur (conj result (vector x y)) (inc x) y)
                         :else (recur result (inc x) y)))]
-    (checker cells 0 0)))
+    (checker ((comp :solid :pixels) cells) 0 0)))
 
-(defn mirror-ship [cells]
-  (distinct (concat (map #(list (nth % 0)  (nth % 1) (nth % 2)) cells) (map #(list (- (dec n) (nth % 0)) (nth % 1) (nth % 2)) cells))))
+(defn mirror-ship2 [cells]
+  (let [pixels (:pixels cells)
+        mirror-list (fn [m]
+                      (concat m (map (fn[[a b]] (vector (- (dec n) a) b)) m)))]
+    (assoc cells :pixels (-> (assoc pixels :hull (mirror-list (:hull pixels )))
+                             (assoc :cockpit (mirror-list (:cockpit pixels )))
+                             (assoc :solid (mirror-list (:solid pixels )))))))
 
-(defn generate-pixel-ship [seed]
-  (-> (add-hull always-solid-tagged seed)
-      (add-cockpit seed)
-      (wrap-with-solids)
-      (mirror-ship)))
+(defn generate-pixel-ship2 [seed]
+  (-> (assoc always-solid2 :seed seed)
+      (add-hull2)
+      (add-cockpit2)
+      (wrap-with-solids2)
+      (mirror-ship2)
+      ))
 
-(generate-pixel-ship 198234)
+(def ship2 (generate-pixel-ship2 Integer/MAX_VALUE))
+(count ((comp :solid :pixels) ship2))
+ship2
+
