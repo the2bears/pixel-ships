19ee7c82cfaeaa39b0de76b2538075f8e1ced89b
William Swaney <the2bears@gmail.com>
2016-06-03 20:48:59 -0700

    Moved inputs for building the ship into a map/model

--- src/pixel_ships/core.clj
+++ src/pixel_ships/core.clj
@@ -1,15 +1,17 @@
 (ns pixel-ships.core)
 
-(def ship-size 12)
-(def always-solid [{:x 5, :y 2} {:x 5, :y 3} {:x 5, :y 4} {:x 5, :y 5} {:x 5, :y 9}])
-(def hull-possibles [{:x 4, :y 1} {:x 5, :y 1} {:x 4, :y 2} {:x 3, :y 3} {:x 4, :y 3} {:x 3, :y 4} {:x 4, :y 4} {:x 2, :y 5} {:x 3, :y 5}
-                     {:x 4, :y 5} {:x 1, :y 6} {:x 2, :y 6} {:x 3, :y 6} {:x 1, :y 7} {:x 2, :y 7} {:x 3, :y 7} {:x 1, :y 8} {:x 2, :y 8}
-                     {:x 3, :y 8} {:x 1, :y 9} {:x 2, :y 9} {:x 3, :y 9} {:x 4, :y 9} {:x 3, :y 10} {:x 4, :y 10} {:x 5, :y 10}])
-
-(def cockpit-possibles [{:x 4, :y 6} {:x 5, :y 6} {:x 4, :y 7} {:x 5, :y 7} {:x 4, :y 8} {:x 5, :y 8}])
+(def bollinger-model
+  {:seed Integer/MAX_VALUE
+   :ship-size 12
+   :model {:solid [{:x 5, :y 2} {:x 5, :y 3} {:x 5, :y 4} {:x 5, :y 5} {:x 5, :y 9}],
+           :cockpit [{:x 4, :y 6} {:x 5, :y 6} {:x 4, :y 7} {:x 5, :y 7} {:x 4, :y 8} {:x 5, :y 8}],
+           :hull [{:x 4, :y 1} {:x 5, :y 1} {:x 4, :y 2} {:x 3, :y 3} {:x 4, :y 3} {:x 3, :y 4} {:x 4, :y 4} {:x 2, :y 5} {:x 3, :y 5}
+                  {:x 4, :y 5} {:x 1, :y 6} {:x 2, :y 6} {:x 3, :y 6} {:x 1, :y 7} {:x 2, :y 7} {:x 3, :y 7} {:x 1, :y 8} {:x 2, :y 8}
+                  {:x 3, :y 8} {:x 1, :y 9} {:x 2, :y 9} {:x 3, :y 9} {:x 4, :y 9} {:x 3, :y 10} {:x 4, :y 10} {:x 5, :y 10}]}})
 
 (defn add-hull [cells]
   (let [seed (:seed cells)
+        hull-possibles ((comp :hull :model) cells)
         hull-count (count hull-possibles)
         checker (fn [result i mask]
                    (cond (>= i hull-count) result
@@ -20,6 +22,7 @@
 
 (defn add-cockpit [cells]
   (let [seed (:seed cells)
+        cockpit-possibles ((comp :cockpit :model) cells)
         cockpit-count (count cockpit-possibles)
         checker (fn [solid-cells cockpit-cells i mask]
                    (cond (>= i cockpit-count)
@@ -33,6 +36,7 @@
 (defn wrap-with-solids [cells]
   (let [hull-cells (into [] ((comp :hull :pixels) cells))
         all-cells (into [] (reduce #(concat %1 %2) (vals (:pixels cells))))
+        ship-size (:ship-size cells)
         checker (fn [result x y]
                   (cond (>= x (/ ship-size 2)) (recur result 0 (inc y))
                         (>= y ship-size) (assoc-in cells [:pixels :solid] (into [] (distinct result)))
@@ -47,21 +51,22 @@
 
 (defn mirror-ship [cells]
   (let [pixels (:pixels cells)
+        ship-size (:ship-size cells)
         mirror-list (fn [m]
                       (into [] (concat m (map (fn[m] {:x (- (dec ship-size) (:x m)) :y (:y m)}) m))))]
     (assoc cells :pixels (-> (assoc pixels :hull (mirror-list (:hull pixels )))
                              (assoc :cockpit (mirror-list (:cockpit pixels )))
                              (assoc :solid (mirror-list (:solid pixels )))))))
 
-(defn generate-pixel-ship [seed]
-  (-> (assoc-in {} [:pixels :solid] always-solid)
-      (assoc :seed seed)
+(defn create-pixel-ship [model]
+  (-> model
+      (assoc-in [:pixels :solid] ((comp :solid :model) model))
       (add-hull)
       (add-cockpit)
       (wrap-with-solids)
       (mirror-ship)
       ))
 
-(def ship (generate-pixel-ship Integer/MAX_VALUE))
-(count ((comp :solid :pixels) ship))
+(def ship (create-pixel-ship bollinger-model))
+(count ((comp :hull :pixels) ship))
 ship
