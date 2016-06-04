7ce5964ae1612a773e6277eec8dfa3c4ba9f0543
William Swaney <the2bears@gmail.com>
2016-06-04 15:19:06 -0700

    Moved default model to new name space

---
+++ src/pixel_ships/bollinger.clj
@@ -0,0 +1,11 @@
+(ns pixel-ships.bollinger)
+
+(def model
+  {:name :bollinger
+   :seed Integer/MAX_VALUE
+   :ship-size 12
+   :model {:solid [{:x 5, :y 2} {:x 5, :y 3} {:x 5, :y 4} {:x 5, :y 5} {:x 5, :y 9}],
+           :cockpit [{:x 4, :y 6} {:x 5, :y 6} {:x 4, :y 7} {:x 5, :y 7} {:x 4, :y 8} {:x 5, :y 8}],
+           :hull [{:x 4, :y 1} {:x 5, :y 1} {:x 4, :y 2} {:x 3, :y 3} {:x 4, :y 3} {:x 3, :y 4} {:x 4, :y 4} {:x 2, :y 5} {:x 3, :y 5}
+                  {:x 4, :y 5} {:x 1, :y 6} {:x 2, :y 6} {:x 3, :y 6} {:x 1, :y 7} {:x 2, :y 7} {:x 3, :y 7} {:x 1, :y 8} {:x 2, :y 8}
+                  {:x 3, :y 8} {:x 1, :y 9} {:x 2, :y 9} {:x 3, :y 9} {:x 4, :y 9} {:x 3, :y 10} {:x 4, :y 10} {:x 5, :y 10}]}})
--- src/pixel_ships/core.clj
+++ src/pixel_ships/core.clj
@@ -1,13 +1,5 @@
-(ns pixel-ships.core)
-
-(def bollinger-model
-  {:seed Integer/MAX_VALUE
-   :ship-size 12
-   :model {:solid [{:x 5, :y 2} {:x 5, :y 3} {:x 5, :y 4} {:x 5, :y 5} {:x 5, :y 9}],
-           :cockpit [{:x 4, :y 6} {:x 5, :y 6} {:x 4, :y 7} {:x 5, :y 7} {:x 4, :y 8} {:x 5, :y 8}],
-           :hull [{:x 4, :y 1} {:x 5, :y 1} {:x 4, :y 2} {:x 3, :y 3} {:x 4, :y 3} {:x 3, :y 4} {:x 4, :y 4} {:x 2, :y 5} {:x 3, :y 5}
-                  {:x 4, :y 5} {:x 1, :y 6} {:x 2, :y 6} {:x 3, :y 6} {:x 1, :y 7} {:x 2, :y 7} {:x 3, :y 7} {:x 1, :y 8} {:x 2, :y 8}
-                  {:x 3, :y 8} {:x 1, :y 9} {:x 2, :y 9} {:x 3, :y 9} {:x 4, :y 9} {:x 3, :y 10} {:x 4, :y 10} {:x 5, :y 10}]}})
+(ns pixel-ships.core
+  (:require [pixel-ships.bollinger :as bollinger]))
 
 (defn add-hull [cells]
   (let [seed (:seed cells)
@@ -67,6 +59,6 @@
       (mirror-ship)
       ))
 
-(def ship (create-pixel-ship bollinger-model))
+(def ship (create-pixel-ship bollinger/model))
 (count ((comp :hull :pixels) ship))
 ship
